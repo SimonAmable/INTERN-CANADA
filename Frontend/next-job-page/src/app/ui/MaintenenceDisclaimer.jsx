f07346fc77dc9c01c30ef807962d1156bf5e31ba
SimonAmable <125688417+SimonAmable@users.noreply.github.com>
2025-06-13 13:50:28 -0400

    string spelling fix

--- Frontend/next-job-page/src/app/ui/MaintenenceDisclaimer.jsx
+++ Frontend/next-job-page/src/app/ui/MaintenenceDisclaimer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MaintenenceDisclaimer = () => {
 return (
     <div className='bg-green-600 text-white text-center p-2 bold font-bold  border-white'>
-    WE ARE FINNALY BACK, thank for visiting after such a long hiatus!!!!!!!!!!!!!!    
+    WE ARE FINNALY BACK, thank you for visiting after such a long hiatus!!!!!!!!!!!!!!    
     </div>
 )
 }
